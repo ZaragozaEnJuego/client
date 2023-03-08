918d3799cf6959e607881c57b42420ce9ca0443c
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-03-08 11:56:03 +0000

    use img

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 
 import './App.css'
 
@@ -7,12 +8,22 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-      <h1 className="text-3xl   font-bold underline">
-      Hello world!
+     
+      <h1 className="text-3xl  font-bold underline">
+      Hello 
+      world!
     </h1>
+    <div className=' flex justify-center' >
+     <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={reactLogo} className="logo" alt="Vite logo" />
+        </a>
+        </div>
+        
     </div>
-    </div>
+   
   )
 }
 
