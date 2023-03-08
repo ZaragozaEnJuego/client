972e7ca99ac537ddcea540e81241b5520b0ab4db
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-03-08 11:48:11 +0000

    init vite & tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <h1 className="text-3xl   font-bold underline">
+      Hello world!
+    </h1>
     </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
