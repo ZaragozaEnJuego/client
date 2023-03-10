722567c08e89a757b2e60e7e03512e0f09f7c2eb
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-03-10 19:00:24 +0000

    no css classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 
-import './App.css';
-
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
-      <h1 className='text-3xl  font-bold underline'>Hello world!</h1>
-      <div className=' flex justify-center'>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={reactLogo} className='logo' alt='Vite logo' />
-        </a>
+    <div className=' flex justify-center'>
+      <div className='flex-col'>
+        <h1 className='text-3xl  font-bold underline'>Hello world!</h1>
+        <div className='flex justify-center mt-10'>
+          <a href='https://vitejs.dev' target='_blank'>
+            <img src='/vite.svg' className='logo' alt='Vite logo' />
+          </a>
+          <a href='https://vitejs.dev' target='_blank'>
+            <img src={reactLogo} className='logo' alt='Vite logo' />
+          </a>
+        </div>
       </div>
     </div>
   );
