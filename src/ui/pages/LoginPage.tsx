216dea2ab4564ce56f8df7410184c9c704a952cf
p3n4x0 <Penacho.AE@gmail.com>
2023-03-27 17:42:39 +0200

    Error, logica planteada

--- src/ui/pages/LoginPage.tsx
+++ src/ui/pages/LoginPage.tsx
@@ -1,22 +1,93 @@
+import { useEffect, useState } from 'react';
+import { ReactComponent as Logo } from '/src/assets/Logo.svg';
+import { ReactComponent as Fondo } from '/src/assets/Fondo.svg';
 import { UseAuth } from '../hooks/auth/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import { LoginLayout } from '../components/layouts/LoginLayout';
 
-const LoginPage = () => {
+const LoginPage = (title = 'ZaragozaEnJuego') => {
   const useAuth = UseAuth();
   const navigate = useNavigate();
+  //a md window have 768 pixels
+  const md = 768;
+
+  //set html head metadata
+  useEffect(() => {
+    document.title = title;
+  }, []);
+
+  //state of the current windw dimension
+  const [windowDimenion, detectHW] = useState({
+    winWidth: window.innerWidth,
+    winHeight: window.innerHeight,
+  });
+
+  //detect size of the current browser window
+  const detectSize = () => {
+    detectHW({
+      winWidth: window.innerWidth,
+      winHeight: window.innerHeight,
+    });
+  };
+
+  //avoid recurrennt executions of detectSize
+  const [mounted, setMounted] = useState(false);
+
+  //run each time the window dimension changes
+  useEffect(() => {
+    //listed al the window redimensions
+    window.addEventListener('resize', detectSize);
+
+    if (!mounted) {
+      setMounted(true);
+      detectSize();
+    }
+
+    return () => {
+      window.removeEventListener('resize', detectSize);
+    };
+  }, [mounted, windowDimenion]);
+
   return (
-    <LoginLayout>
-      <button
-        onClick={() => {
-          useAuth.handleLogin();
-          navigate('/', { replace: true });
-        }}
-        className='px-20 py-4 mt-4 bg-primary text-secondary rounded-lg'
-      >
-        Iniciar sesión
-      </button>      
-    </LoginLayout>
+    <>
+      {windowDimenion.winWidth > md ? (
+        //Desktop layout
+        <div className=' w-screen h-screen bg-background '>
+          <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'></div>
+          <div className='bg-secondary bg-opacity-80 p-8 rounded-lg'>
+            <h1 className='text-5xl font-bold text-primary'>{title}</h1>
+            <Logo className='my-8'/>
+            <button
+              onClick={() => {
+                useAuth.handleLogin();
+                navigate('/', { replace: true });
+              }}
+              className='px-20 py-4 mt-4 bg-primary text-secondary rounded-lg'
+            >
+              Iniciar sesión
+            </button>
+          </div>
+        </div>
+      ) : (
+        //Mobile layout
+        <div className=' w-screen h-screen bg-background '>
+          <Fondo className='absolute w-full h-full top-0 left-0 z-0' />
+          <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'></div>
+          <div className='bg-secondary bg-opacity-80 p-8 rounded-lg'>
+          <h1 className='text-5xl font-bold text-primary'>{title}</h1>
+            <Logo className='my-8'/>
+            <button
+              onClick={() => {
+                useAuth.handleLogin();
+                navigate('/', { replace: true });
+              }}
+              className='px-20 py-4 mt-4 bg-primary text-secondary rounded-lg'
+            >
+              Iniciar sesión
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
