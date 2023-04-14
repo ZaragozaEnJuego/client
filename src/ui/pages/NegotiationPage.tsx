26294fb5cc9ea9304c8b545987090ea3e72d9ee2
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-04-14 13:41:53 +0200

    fixed (#35)

--- src/ui/pages/NegotiationPage.tsx
+++ src/ui/pages/NegotiationPage.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import { OfferList, UserOfferList } from '../components/ui/offer';
 
 const NegotiationPage = () => {
-  const offerRepo: IOfferRepo = new MemoryOfferRepo(); 
+  const offerRepo: IOfferRepo = new MemoryOfferRepo();
   const [offersList, setOffersList] = useState<Offer[]>([]);
   const [userOffersList, setUserOffersList] = useState<Offer[]>([]);
 
@@ -20,20 +20,20 @@ const NegotiationPage = () => {
   }, []);
 
   const getOwnerOffers = async () => {
-    const data = await fetch('')  //Ruta correspondiente
-    const offers = await data.json()
-    console.log('Ofertas a las propiedades del usuario recibidas')
-  }
+    const data = await fetch(''); //Ruta correspondiente
+    const offers = await data.json();
+    console.log('Ofertas a las propiedades del usuario recibidas');
+  };
 
   const getOffererOffers = async () => {
-    const data = await fetch('')  //Ruta correspondiente
-    const offers = await data.json()
-    console.log('Ofertas del usuario recibidas')
-  }
+    const data = await fetch(''); //Ruta correspondiente
+    const offers = await data.json();
+    console.log('Ofertas del usuario recibidas');
+  };
 
   return (
     <MainLayout>
-      <div className='xl:flex flex-col-2 justify-start pb-40 mx-10 w-auto'>
+      <div className='xl:flex flex-col-2 justify-start pb-40 mx-10 w-auto h-full'>
         <div className='font-bold text-primary items-start text-3xl h-full w-full ml-5 flex flex-col'>
           <h1>Mis Ofertas</h1>
           <UserOfferList list={userOffersList} />
