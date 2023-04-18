1288bf7cb2bbf186554421585a4846cb91ce7247
Ibón González Martínez de Apellániz <igonzalezma7@gmail.com>
2023-04-18 03:55:54 +0200

    Editar colores charts

--- src/ui/components/ui/user/DonutChart.tsx
+++ src/ui/components/ui/user/DonutChart.tsx
@@ -22,20 +22,20 @@ export function DonutChart({ data, labels }: DonutChartProps) {
               label: 'vendidas',
               data: data,
               backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                '#BF616A',
+                '#5E81AC',
+                '#EBCB8B',
+                '#A3BE8C',
+                '#B48EAD',
+                '#D08770'
               ],
               borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                '#BF616A',
+                '#5E81AC',
+                '#EBCB8B',
+                '#A3BE8C',
+                '#B48EAD',
+                '#D08770'
               ],
               borderWidth: 1
             }]
