86b9c16d67041a7210945ece1aa9a72ad70f0893
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-04-29 11:11:44 +0000

    delete unusued things

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
-import { MainLayout } from './ui/components/layouts';
 import { router } from './router';
 import { AuthProvider } from './ui/hooks/auth/AuthProvider';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <AuthProvider>
       <RouterProvider router={router} />
