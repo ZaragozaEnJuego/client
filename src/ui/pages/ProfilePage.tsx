2284c2800a14cb2f9a156ad8a39f79b0ba444aa4
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-05-08 08:03:00 +0000

    delete unusued field

--- src/ui/pages/ProfilePage.tsx
+++ src/ui/pages/ProfilePage.tsx
@@ -42,7 +42,6 @@ const ProfilePage = () => {
         <div className='w-full md:w-1/2 flex flex-col h-full '>
           <div className=' md:h-1/3 md:min-h-fit  w-full flex  items-center justify-center '>
             <div className='rounded-3xl border-secondary border-2 text-primary font-bold  text-lg md:text-3xl p-3 w-full mx-4   flex flex-col  items-center justify-center '>
-              <h1>Balance ultimo día: {landlord.lastDayIncome}</h1>
               <h1>Patrimonio: {calcPropertieValue(landlord.properties)}</h1>
               <h1>Liquidez: {landlord.liquidity}</h1>
             </div>
