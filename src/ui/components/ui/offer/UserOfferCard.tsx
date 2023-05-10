b59c5595857b0c0e8ba52b851967be19a240faf7
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-05-10 20:44:47 +0200

    Revert "Revert "Negotiation page upgrade style (#37)" (#49)"
    This reverts commit d6c09d001b50784129f6f38697e23859b14ae521.

--- src/ui/components/ui/offer/UserOfferCard.tsx
+++ src/ui/components/ui/offer/UserOfferCard.tsx
@@ -13,16 +13,16 @@ interface Offers {
 const PropertyIcon = (kind: Kind) => {
   switch (kind) {
     case 'health':
-      return <MedicalIcon style={{ fill: '#2E3440' }} className={'h-full  w-16 px-2'} />;
+      return <MedicalIcon style={{ fill: '#2E3440' }} className={'h-full w-10 lg:w-16 px-1 sm:px-2'} />;
 
     case 'groceries':
-      return <GrocerieIcon style={{ fill: '#2E3440' }} className={'h-full  w-16 px-2'} />;
+      return <GrocerieIcon style={{ fill: '#2E3440' }} className={'h-full w-10 lg:w-16 px-1 sm:px-2'} />;
 
     case 'education':
-      return <SchoolIcon style={{ fill: '#2E3440' }} className={'h-full  w-16 px-2'} />;
+      return <SchoolIcon style={{ fill: '#2E3440' }} className={'h-full w-10 lg:w-16 px-1 sm:px-2'} />;
 
     case 'transport':
-      return <TrainIcon style={{ fill: '#2E3440' }} className={'h-full  w-16 px-2'} />;
+      return <TrainIcon style={{ fill: '#2E3440' }} className={'h-full w-10 lg:w-16 px-1 sm:px-2'} />;
     default:
       return <></>;
   }
@@ -30,48 +30,26 @@ const PropertyIcon = (kind: Kind) => {
 
 const UserOfferCard: FC<Offers> = ({ offer }) => {
   return (
-    <div
-      style={{ background: '#8FBCBB' }}
-      className='flex justify-center items-center border w-full rounded-3xl py-2 px-4 my-4 h-100'
-    >
-      <div className='flex flex-col justify-center items-center'>
-        <img
-          className='w-10 h-10 object-cover rounded-full '
-          src='https://miro.medium.com/v2/resize:fit:785/0*Ggt-XwliwAO6QURi.jpg'
-        />
-        <h1 className='text-xs text-nord1'>Tú</h1>
-        <h1 className='text-xl w-50 font-bold text-primary'>{offer.amount}€</h1>
-      </div>
-      <div className='flex items-center justify-top mt-4 mx-4 flex-col'>
-        {PropertyIcon('transport')}
-        <div className='flex flex-col items-bottom px-5 py-1 w-full'>
-          <button
-            className='items-center text-xs text-hover py-1 my-1 mx-2 w-20 rounded-lg bg-primary'
-            onClick={() => {
-              alert('Oferta cancelada');
-            }}
-          >
-            Cancelar
-          </button>
-          <button
-            style={{ background: '#8FBCBB', color: '#8FBCBB' }}
-            className='items-center text-xs cursor-default py-1 my-1 w-20 rounded-lg mx-2'
-          >
-            Consultar
-          </button>
-        </div>
-      </div>
-      <div className='flex flex-col justify-center items-center '>
-        <img
-          className='w-10 h-10 object-cover rounded-full '
-          src='https://media.istockphoto.com/id/1151155288/es/foto/close-up-foto-incre%C3%ADble-hermosa-ella-sus-dedos-pulgar-de-la-dama-indican-directamente-pecho.jpg?s=612x612&w=0&k=20&c=NCuogVJLvUYYfMihKzWPAl7OPRQSsQsyWFKi6fyuty4='
-        />
-        <h1 className='text-xs text-nord1'>Lucía</h1>
-        <h1 style={{ background: '#8FBCBB' }} className={'w-50 font-bold text-lg'}>
-          Estación Delicias
-        </h1>
+    <div style={{ background: '#8FBCBB' }} className='flex justify-center align-middle items-center overflow-x-clip border w-auto rounded-3xl py-2 px-1 sm:px-4 my-4 h-100'>
+    <div className='flex flex-col justify-center items-center'>
+      <img className='w-10 h-10 object-cover rounded-full 'src='https://miro.medium.com/v2/resize:fit:785/0*Ggt-XwliwAO6QURi.jpg'/>
+      <h1 className='text-xs text-nord1' >Tú</h1>
+      <h1 className='text-xs lg:text-lg md:text-base sm:text-sm xl:text-xl w-50 font-bold text-primary text-center'>{offer.amount}€</h1>
+    </div>
+    <div className='flex items-center justify-top mt-4 mx-1 sm:mx-2 md:mx-4 flex-col'>
+      {PropertyIcon('transport')}
+      <div className='flex flex-col items-bottom px-5 py-1 w-full'>
+        <button className='items-center text-xs text-hover py-1 my-1 sm:mx-2 mx-1 w-20 rounded-lg bg-primary'
+          onClick={() => { alert('Oferta cancelada');}}>Cancelar</button>
+        <button style={{ background: '#8FBCBB', color: '#8FBCBB'}} className='items-center text-xs cursor-default py-1 my-1 w-20 rounded-lg mx-2'>Consultar</button>
       </div>
     </div>
-  );
+    <div className='flex flex-col justify-center items-center '>
+      <img className='w-10 h-10 object-cover rounded-full 'src='https://media.istockphoto.com/id/1151155288/es/foto/close-up-foto-incre%C3%ADble-hermosa-ella-sus-dedos-pulgar-de-la-dama-indican-directamente-pecho.jpg?s=612x612&w=0&k=20&c=NCuogVJLvUYYfMihKzWPAl7OPRQSsQsyWFKi6fyuty4='/>
+      <h1 className='text-xs text-nord1' >Lucía</h1>
+      <h1 style={{ background: '#8FBCBB' }} className={'w-50 text-center font-bold text-sm lg:text-base md:text-sm xl:text-lg'}>Estación Delicias</h1>
+    </div>
+  </div>
+    );
 };
 export { UserOfferCard };
--- src/ui/pages/NegotiationPage.tsx
+++ src/ui/pages/NegotiationPage.tsx
@@ -8,6 +8,8 @@ const NegotiationPage = () => {
   const offerRepo: IOfferRepo = new MemoryOfferRepo();
   const [offersList, setOffersList] = useState<Offer[]>([]);
   const [userOffersList, setUserOffersList] = useState<Offer[]>([]);
+  //a md window have 768 pixels
+  const md = 768;
 
   useEffect(() => {
     offerRepo.getOwnerOffers().then((list) => {
@@ -18,6 +20,38 @@ const NegotiationPage = () => {
     });
     //TODO: Modificar este useEffect para que llame a las operaciones que reciben los datos de la api
   }, []);
+
+  //state of the current windw dimension
+  const [windowDimenion, detectHW] = useState({
+    winWidth: window.innerWidth,
+    winHeight: window.innerHeight,
+  });
+
+  //detect size of the current browser window
+  const detectSize = () => {
+    detectHW({
+      winWidth: window.innerWidth,
+      winHeight: window.innerHeight,
+    });
+  };
+
+  //avoid recurrennt executions of detectSize
+  const [mounted, setMounted] = useState(false);
+
+  //run each time the window dimension changes
+  useEffect(() => {
+    //listed al the window redimensions
+    window.addEventListener('resize', detectSize);
+
+    if (!mounted) {
+      setMounted(true);
+      detectSize();
+    }
+
+    return () => {
+      window.removeEventListener('resize', detectSize);
+    };
+  }, [mounted, windowDimenion]);
 
   const getOwnerOffers = async () => {
     const data = await fetch(''); //Ruta correspondiente
@@ -31,19 +65,76 @@ const NegotiationPage = () => {
     console.log('Ofertas del usuario recibidas');
   };
 
+  const [showOffers, setShowOffers] = useState(false);
+  const [buttonText, setButtonText] = useState('Mis propiedades');
+
+  const toggleShowOffers = () => {
+    setShowOffers(!showOffers);
+    if(!showOffers) {
+      setButtonText('Mis ofertas');
+    } else {
+      setButtonText('Mis propiedades');
+    }
+  };
+
+  const userComponent = (
+    <div className='font-bold text-primary items-start text-3xl h-full md:w-1/2 w-full ml-2 md:ml-5 flex flex-col'>
+      <h1>Mis propiedades</h1>
+      <OfferList list={offersList} />
+    </div>
+  );
+
+  const offerComponent = (
+    <div className='font-bold text-primary items-start text-3xl h-full w-full md:w-1/2 xs:w-full ml-2 md:ml-5 md:mr-5 flex flex-col'>
+      <h1>Mis ofertas</h1>
+      <UserOfferList list={userOffersList} />
+    </div>
+  );
+
   return (
-    <MainLayout>
-      <div className='xl:flex flex-col-2 justify-start pb-40 mx-10 w-auto h-full'>
-        <div className='font-bold text-primary items-start text-3xl h-full w-full ml-5 flex flex-col'>
-          <h1>Mis Ofertas</h1>
-          <UserOfferList list={userOffersList} />
-        </div>
-        <div className='font-bold text-primary items-start text-3xl h-full w-full ml-5 flex flex-col'>
-          <h1>Mis propiedades</h1>
-          <OfferList list={offersList} />
-        </div>
-      </div>
-    </MainLayout>
+    <>
+      {windowDimenion.winWidth > md ? (
+        //Desktop layout
+        <MainLayout>
+          <div className='overflow-y-scroll overflow-x-clip pr-2 h-full '>
+            <div className='sm:flex flex-col-2 justify-start pb-40 mr-2 sm:mr-5 w-auto h-full'>
+              <div className='font-bold text-primary items-start text-3xl h-full md:w-1/2 w-full ml-2 md:ml-5 flex flex-col'>
+                <h1>Mis ofertas</h1>
+                <UserOfferList list={userOffersList} />
+              </div>
+              <div className='font-bold text-primary items-start text-3xl h-full w-full md:w-1/2 xs:w-full ml-2 md:ml-5 md:mr-5 flex flex-col'>
+                <h1>Mis propiedades</h1>
+                <OfferList list={offersList} />
+              </div>
+            </div>
+          </div>
+        </MainLayout>
+      ) : (
+        //Mobile layout
+        <MainLayout>
+          <div className='overflow-y-scroll overflow-x-clip pr-2 h-full '>
+            <div className='flex justify-end items-center mb-4'>
+              <span className='mr-2 font-bold text-primary'>{buttonText}</span>
+              <div className='relative inline-block w-10 mr-2 align-middle select-none'>
+                <input
+                  type='checkbox'
+                  name='toggle'
+                  id='toggle'
+                  className='toggle-checkbox absolute block w-6 h-6 rounded-full bg-[#5E81AC] border-4 appearance-none cursor-pointer'
+                  checked={showOffers}
+                  onChange={toggleShowOffers}
+                />
+                <label
+                  htmlFor='toggle'
+                  className='toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer'
+                ></label>
+              </div>
+            </div>
+            {showOffers ? offerComponent : userComponent}
+          </div>
+        </MainLayout>
+      )}
+    </>
   );
 };
 
