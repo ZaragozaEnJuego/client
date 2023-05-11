83fb76e84f9570bca6af3a50b0599f24d9b53d87
Ibón González Martínez de Apellániz <igonzalezma7@gmail.com>
2023-05-11 20:35:58 +0200

    Update AdminStatsRepo.ts

--- src/infraestructure/http/AdminStatsRepo.ts
+++ src/infraestructure/http/AdminStatsRepo.ts
@@ -7,7 +7,40 @@ export class HTTPAdminStatsRepo {
         return ''
     }
     async getPropertiesByKind(kind: Kind): Promise<Propertie[]> {
-        return []
+        interface PropertieDTO {
+            _id: string;
+            name: string;
+            kind: 'transport' | 'education' | 'health' | 'groceries';
+            address: string;
+            lng: number;
+            lat: number;
+            price: number;
+            baseIncome: number;
+            owener?: string;
+        }
+        const response = await axios.get<PropertieDTO[]>(`/statsadmin/${kind}`, {
+            headers: {
+                accept: 'application/json'
+            },
+        })
+        if (response.status !== 200) {
+            throw new Error('No se puedo obtener los datos de las propiedades');
+          }
+          console.log(response.data);
+          return response.data.map((propertieDto) => {
+            const prop: Propertie = {
+              address: propertieDto.address,
+              id: propertieDto._id,
+              income: propertieDto.baseIncome,
+              kind: propertieDto.kind,
+              lat: propertieDto.lat,
+              lng: propertieDto.lng,
+              name: propertieDto.name,
+              price: propertieDto.price,
+              owner: propertieDto.owener,
+            };
+            return prop;
+          });
     }
     async collectPurchaseInfo(idProperty: string, date: Date, kind: Kind): Promise<string> {
         return ''
