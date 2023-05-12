4c0dfb85f0e09a2ab37b023dca51483332747113
Ibón González Martínez de Apellániz <igonzalezma7@gmail.com>
2023-05-12 18:16:52 +0200

    offers

--- src/ui/components/ui/offer/OfferList.tsx
+++ src/ui/components/ui/offer/OfferList.tsx
@@ -12,63 +12,63 @@ interface Offers {
     list: Offer[];
     size?: Size
   }
-const params = useParams()
-const userRepo: HTTPAdminRepo = new HTTPAdminRepo()
-const [ offerer, setOfferer ] = useState<User>(defaultUser())
-const [ owner, setOwner ] = useState<User>(defaultUser())
-
-useEffect(() => {
-    if (params.buildingId !== undefined) {
-        try {
-            userRepo.getUser(params.buildingId).then((offerer: User) => {
-                console.log(offerer)
-                setOfferer(offerer)
-            })
-        } catch (error) {
-            toast('Error al obtener datos del oferente', {
-                position: 'top-right',
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: 'light',
-              })
-        }
-    }
-})
-
-useEffect(() => {
-    if (params.buildingId !== undefined) {
-        try {
-            userRepo.getUser(params.buildingId).then((owner: User) => {
-                console.log(owner)
-                setOwner(owner)
-            })
-        } catch (error) {
-            toast('Error al obtener datos del propietario', {
-                position: 'top-right',
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: 'light',
-              })
-        }
-    }
-})
 
 const OfferList: FC<Offers> = ({ list, size }) => {
+    const params = useParams()
+    const userRepo: HTTPAdminRepo = new HTTPAdminRepo()
+    const [ offerer, setOfferer ] = useState<User>(defaultUser())
+    const [ owner, setOwner ] = useState<User>(defaultUser())
+
+    useEffect(() => {
+        if (params.buildingId !== undefined) {
+            try {
+                userRepo.getUser(params.buildingId).then((offerer: User) => {
+                    console.log(offerer)
+                    setOfferer(offerer)
+                })
+            } catch (error) {
+                toast('Error al obtener datos del oferente', {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'light',
+              })
+        }
+    }
+})
+
+    useEffect(() => {
+        if (params.buildingId !== undefined) {
+            try {
+                userRepo.getUser(params.buildingId).then((owner: User) => {
+                    console.log(owner)
+                    setOwner(owner)
+                })
+            } catch (error) {
+                toast('Error al obtener datos del propietario', {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'light',
+                })
+            }
+        }
+    })
     return (
         <div className='w-full overflow-y-scroll overflow-x-clip pr-2 h-full'>
         {size === 'small' ? (
-            list.map((value, index) => (
+            list.map((value) => (
                 <SmallOfferCard offer={value} />))
         ) : (
-            list.map((value, index) => (
+            list.map((value) => (
                 <OfferCard offer={value} offerer={offerer} owner={owner} />))
         )}
         </div>
