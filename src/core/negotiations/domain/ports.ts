63fc06f2e15eadd514f848532d0661a17ee9009f
Ibón González Martínez de Apellániz <igonzalezma7@gmail.com>
2023-05-14 22:37:02 +0200

    crear oferta

--- src/core/negotiations/domain/ports.ts
+++ src/core/negotiations/domain/ports.ts
@@ -3,4 +3,7 @@ import { Offer } from './model';
 export interface IOfferRepo {
   getOwnerOffers(): Promise<Offer[]>;
   getOffererOffers(): Promise<Offer[]>;
+  createOffer(_property: string, _owner: string, _offerer: string, _amount: number): Promise<string>;
+  execOffer(offerId: string): Promise<string>;
+  deleteOffer(offerId: string): Promise<string>;
 }
---
+++ src/infraestructure/http/OfferRepo.ts
@@ -0,0 +1,98 @@
+import { Offer } from '../../core/negotiations/domain'
+import axios from './http'
+
+export class HTTPOfferRepo {
+    async createOffer(_property: string, _owner: string, _offerer: string, _amount: number): Promise<string> {
+        const response = await axios.post('/negotiations/create', {
+            property: _property,
+            owner: _owner,
+            offerer: _offerer,
+            amount: _amount
+        })
+        if (response.status !== 201) {
+            throw new Error('No se puedo obtener los datos de la oferta creada')
+        }
+        console.log(response.data)
+        return response.data.id
+    }
+    async getOffererOffers(id: string): Promise<Offer[]> {
+        interface OfferDTO {
+            _id: string,
+            property: string,
+            offerer: string,
+            owner: string,
+            amount: number
+        }
+        const response = await axios.get<OfferDTO[]>(`/negotiation/${id}/offerer`, {
+            headers: {
+                accept: 'application/json'
+            }
+        })
+        if (response.status !== 200) {
+            throw new Error('Error al obtener las ofertas del servidor');
+        }
+        console.log(response.data)
+        return response.data.map((offerDTO => {
+            const offer: Offer = {
+                id: offerDTO._id,
+                property: offerDTO.property,
+                offerer: offerDTO.offerer,
+                owner: offerDTO.owner,
+                amount: offerDTO.amount
+            }
+            return offer
+        }))
+    }
+    async getOwnerOffers(id: string): Promise<Offer[]> {
+        interface OfferDTO {
+            _id: string,
+            property: string,
+            offerer: string,
+            owner: string,
+            amount: number
+        }
+        const response = await axios.get<OfferDTO[]>(`/negotiation/${id}/owner`, {
+            headers: {
+                accept: 'application/json'
+            }
+        })
+        if (response.status !== 200) {
+            throw new Error('Error al obtener las ofertas del servidor');
+        }
+        console.log(response.data)
+        return response.data.map((offerDTO => {
+            const offer: Offer = {
+                id: offerDTO._id,
+                property: offerDTO.property,
+                offerer: offerDTO.offerer,
+                owner: offerDTO.owner,
+                amount: offerDTO.amount
+            }
+            return offer
+        }))
+    }
+    async execOffer(offerId: string): Promise<string> {
+        const response = await axios.post(`/negotiation/${offerId}/execute`, {
+            headers: {
+                accept: 'application/json'
+            }
+        })
+        if (response.status !== 201) {
+            throw new Error('Error al ejecutar la oferta')
+        }
+        console.log(response.data)
+        return response.data.id
+    }
+    async deleteOffer(offerId: string): Promise<string> {
+        const response = await axios.delete(`/negotiation/${offerId}/delete`, {
+            headers: {
+                accept: 'application/json'
+            }
+        })
+        if (response.status !== 201) {
+            throw new Error('Error al eliminar la oferta')
+        }
+        console.log(response.data)
+        return response.data.id
+    }
+}
