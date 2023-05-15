85abc583907ff71835632ad9b4cc6ece36b4ac58
Ibón González Martínez de Apellániz <41486432+ibon-sk@users.noreply.github.com>
2023-05-16 00:06:57 +0200

    Update ports.ts

--- src/core/negotiations/domain/ports.ts
+++ src/core/negotiations/domain/ports.ts
@@ -3,7 +3,7 @@ import { Offer } from './model';
 export interface IOfferRepo {
   getOwnerOffers(id: string): Promise<Offer[]>;
   getOffererOffers(id: string): Promise<Offer[]>;
-  createOffer(_property: string, _owner: string, _offerer: string, _amount: number): Promise<string>;
+  createOffer(_property: string, _offerer: string, _amount: number): Promise<string>;
   execOffer(offerId: string): Promise<string>;
   deleteOffer(offerId: string): Promise<string>;
 }
