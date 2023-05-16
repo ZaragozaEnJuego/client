fa3a2e56b7116a18eda386aa2f8319265fac515a
Ibón González Martínez de Apellániz <igonzalezma7@gmail.com>
2023-05-16 14:24:17 +0200

    funcionalidad adminpage

--- src/ui/components/ui/user/UserList.tsx
+++ src/ui/components/ui/user/UserList.tsx
@@ -1,15 +1,19 @@
 import { FC } from 'react';
 import { UserCard } from '.';
 import { User } from '../../../../core/admin/domain';
+import { UseAuth } from '../../../hooks/auth/AuthContext';
 
 interface Users {
     list: User[];
 }
 
 const UserList: FC<Users> = ({ list }) => {
+    const auth = UseAuth()
+    const adminId = auth.getUserId()
+
     return (
         <div className='w-full overflow-y-scroll overflow-x-clip pr-2 h-full '>
-            {list.map((value) => (<UserCard user={value} />))}
+            {list.map((value) => value._id === adminId ? null : (<UserCard user={value}/>))}
         </div>
     );
 }
